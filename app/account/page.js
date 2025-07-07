92b1538aebf0f9bd54efa8c3b713e1a39ecb56a7
mikkelsons <mikkelson_shawn@yahoo.com>
2025-07-06 21:36:36 -0400

    implement NextAuth with Google OAuth, connect to supabase

--- app/account/page.js
+++ app/account/page.js
@@ -1,12 +1,19 @@
+import { auth } from "@/app/_lib/auth";
+
 export const metadata = {
   title: "Guest Area",
 };
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+  console.log(session);
+
+  const firstName = session.user.name.split(" ").at(0);
+
   return (
     <div>
       <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-        Welcome Jonas
+        Welcome, {firstName}
       </h2>
     </div>
   );
